b1951962a6919d0b89870749154518ffc099eacb
Heitor negromonte <heiitornoc@gmail.com>
2023-02-15 15:54:54 -0300

    implementando useRouter

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -4,6 +4,7 @@ import { ArrowRight } from "phosphor-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from "next/router";
 
 const ClaimUsernameFormSchema = z.object({
     username: z.string()
@@ -15,12 +16,16 @@ const ClaimUsernameFormSchema = z.object({
 type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
-    const { register, handleSubmit, formState: { errors } } = useForm<ClaimUsernameFormData>({
+    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<ClaimUsernameFormData>({
         resolver: zodResolver(ClaimUsernameFormSchema),
     })
 
-    async function handleClaimUsername(data: ClaimUsernameFormData) {
+    const router = useRouter()
 
+    async function handleClaimUsername(data: ClaimUsernameFormData) {
+        const { username } = data
+
+        await router.push(`/register?username=${username}`)
     }
 
     return (
@@ -33,7 +38,7 @@ export function ClaimUsernameForm() {
                     placeholder="seu-usuario"
                     {...register('username')}
                 />
-                <Button size="sm" type="submit">
+                <Button size="sm" type="submit" disabled={isSubmitting}>
                     Reservar
                     <ArrowRight />
                 </Button>
