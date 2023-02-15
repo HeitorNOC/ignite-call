d3f300755ea409b1a00a7c97e8e6ec5618073476
Heitor negromonte <heiitornoc@gmail.com>
2023-02-15 17:11:14 -0300

    validacoes do prisma e cookies

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -6,6 +6,8 @@ import { z } from "zod";
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { api } from "../../lib/axios";
+import { AxiosError } from "axios";
 
 const registerFormSchema = z.object({
     username: z.string()
@@ -37,7 +39,19 @@ export default function Register() {
     }, [router.query?.username, setValue])
 
     async function handleRegister(data: RegisterFormData) {
+        try {
+            await api.post('/users', {
+                name: data.name,
+                username: data.username
+            })
+        } catch (err) {
+            if (err instanceof AxiosError && err?.response?.data?.message) {
+                alert(err.response.data.message)
+                return
+            }
 
+            console.error(err)
+        }
     }
 
     return (
