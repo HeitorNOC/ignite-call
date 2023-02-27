6e1b0134b6ff859d8c5bae54118ae4f8c32a7884
Heitor negromonte <heiitornoc@gmail.com>
2023-02-27 17:35:51 -0300

    chamada api para cadastrar time-intervals no banco

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -7,6 +7,7 @@ import { z } from "zod";
 import { getWeekDays } from "../../../utils/get-week-days";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { convertTimeStringToMinutes } from "../../../utils/convert-time-string-to-minutes";
+import { api } from "../../../lib/axios";
 
 const timeIntervalsSchema = z.object({
   intervals: z.array(
@@ -74,9 +75,11 @@ export default function TimeIntervals() {
   const intervals = watch('intervals')
 
   async function handleSetTimeIntervals(data: any) {
-    const formData = data as TimeIntervalsFormOutput
+    const { intervals } = data as TimeIntervalsFormOutput
 
-    console.log(formData)
+    await api.post('/users/time-intervals', {
+      intervals
+    })
   }
 
   return (
