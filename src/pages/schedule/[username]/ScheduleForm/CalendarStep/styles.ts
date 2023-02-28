f1164413732662d7b54eee6af646c4d9edabe2cd
Heitor negromonte <heiitornoc@gmail.com>
2023-02-28 15:51:40 -0300

    estilizacao do calendario

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
@@ -4,5 +4,8 @@ export const Container = styled(Box, {
   margin: "$6 auto 0",
   padding: 0,
   display: "grid",
-  maxWidth: "100%"
+  maxWidth: "100%",
+
+  width: 540,
+  gridTemplateColumns: "1fr"
 })
