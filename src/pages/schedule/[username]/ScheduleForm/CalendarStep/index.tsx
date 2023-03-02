6c23f5995cd1537952751bda55da1d8042c19c2c
Heitor negromonte <heiitornoc@gmail.com>
2023-03-02 00:50:44 -0300

    utilizando react-query

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Calendar } from "../../../../../components/Calendar";
 import { Container, TimePicker, TimePickerHeader, TimePickerItem, TimePickerList } from "./styles";
 import dayjs from "dayjs";
 import { useRouter } from "next/router";
 import { api } from "../../../../../lib/axios";
+import { useQuery } from "@tanstack/react-query";
 
 interface Availability {
   possibleTimes: number[]
@@ -12,7 +13,6 @@ interface Availability {
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
-  const [availability, setAvailability] = useState<Availability | null>(null)
 
   const router = useRouter()
 
@@ -21,26 +21,28 @@ export function CalendarStep() {
 
   const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
   const describedDate = selectedDate ? dayjs(selectedDate).format('DD[ de ]MMMM') : null
-  
-  useEffect(() => {
-    if (!selectedDate) {
-      return 
-    }
 
-    api.get(`/users/${username}/availability`, {
+  const selectedDateWithoutTime = selectedDate ? dayjs(selectedDate).format('YYYY-MM-DD') : null
+
+  const { data: availability } = useQuery<Availability>(['availability', selectedDateWithoutTime],
+   async () => {
+    const response = await api.get(`/users/${username}/availability`, {
       params: {
-        date: dayjs(selectedDate).format('YYYY-MM-DD')
+        date: selectedDateWithoutTime
       }
     })
-    .then((response) => {
-      setAvailability(response.data)
-    })
-  }, [selectedDate, username])
+
+    return response.data
+  },
+  {
+    enabled: !!selectedDate
+  }
+  )
 
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
-      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate}/>
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
 
       {isDateSelected && (
         <TimePicker>
