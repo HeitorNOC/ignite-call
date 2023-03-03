4cb3a40b7e273d0825d6c5c932b0f116d671fbdf
Heitor negromonte <heiitornoc@gmail.com>
2023-03-03 18:50:19 -0300

    refresh token do google

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -12,6 +12,9 @@ export function buildNextAuthOptions(req: NextApiRequest | NextPageContext['req'
         clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
         authorization: {
           params: {
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
             scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar'
           }
         },
